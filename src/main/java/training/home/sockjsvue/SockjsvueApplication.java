87b98e64155bfaed516909ac25b3deee4c1e0820
Marcel <marcel@marcel-debian-9.fritz.box>
2018-12-04 19:37:33 +0100

    init

---
+++ src/main/java/training/home/sockjsvue/SockjsvueApplication.java
@@ -0,0 +1,12 @@
+package training.home.sockjsvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockjsvueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockjsvueApplication.class, args);
+	}
+}
